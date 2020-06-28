fe7c2fe38bab1a633e93c138d007532dba61f4e0
usang <usang6325@gmail.com>
2020-06-28 19:06:33 +0900

    refactoring HttpResponse

--- web-application-server/src/test/java/http/HttpRequestTest.java
+++ web-application-server/src/test/java/http/HttpRequestTest.java
@@ -1,4 +1,4 @@
-package util;
+package http;
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,18 +13,17 @@ import http.HttpRequest;
 public class HttpRequestTest {
 	private String testDirectory = "./src/test/resources/";
 	
-	/*
-	 * @Test public void request_GET() throws Exception { InputStream in = new
-	 * FileInputStream(new File(testDirectory + "Http_GET.txt")); HttpRequest
-	 * request = new HttpRequest(in);
-	 * 
-	 * assertEquals("GET", request.getMethod()); assertEquals("/user/create",
-	 * request.getPath()); assertEquals("keep-alive",
-	 * request.getHeader("Connection")); assertEquals("usang31",
-	 * request.getParameter("userId"));
-	 * 
-	 * }
-	 */
+	  @Test 
+	  public void request_GET() throws Exception { InputStream in = new
+		  FileInputStream(new File(testDirectory + "Http_GET.txt")); HttpRequest
+		  request = new HttpRequest(in);
+		  
+		  assertEquals("GET", request.getMethod()); assertEquals("/user/create",
+		  request.getPath()); assertEquals("keep-alive",
+		  request.getHeader("Connection")); assertEquals("usang31",
+		  request.getParameter("userId"));
+	  }
+	 
 	
 	@Test
 	public void request_POST() throws Exception {
