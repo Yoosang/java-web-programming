a7428fd8e3c067b427b58aebde4eb5b766df9fd6
usang <usang6325@gmail.com>
2020-08-23 20:17:54 +0900

    request line refactoring

--- web-application-server/src/test/java/http/HttpRequestTest.java
+++ web-application-server/src/test/java/http/HttpRequestTest.java
@@ -14,14 +14,14 @@ public class HttpRequestTest {
 	private String testDirectory = "./src/test/resources/";
 	
 	  @Test 
-	  public void request_GET() throws Exception { InputStream in = new
-		  FileInputStream(new File(testDirectory + "Http_GET.txt")); HttpRequest
-		  request = new HttpRequest(in);
+	  public void request_GET() throws Exception { 
+		  InputStream in = new FileInputStream(new File(testDirectory + "Http_GET.txt")); 
+		  HttpRequest request = new HttpRequest(in);
 		  
-		  assertEquals("GET", request.getMethod()); assertEquals("/user/create",
-		  request.getPath()); assertEquals("keep-alive",
-		  request.getHeader("Connection")); assertEquals("usang31",
-		  request.getParameter("userId"));
+		  assertEquals("GET", request.getMethod()); 
+		  assertEquals("/user/create", request.getPath()); 
+		  assertEquals("keep-alive", request.getHeader("Connection")); 
+		  assertEquals("usang31", request.getParameter("userId"));
 	  }
 	 
 	
